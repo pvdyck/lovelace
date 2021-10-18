fd0d1090610fc24b320d617ef898419f53cb8be2
Pierre Schweiger <schweiger.pierre@gmail.com>
2021-10-18 17:12:56 +0700

    Add seo and analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,16 +38,41 @@ if (!spaceId || !accessToken) {
 module.exports = {
   siteMetadata: {
     title: "Lovelace",
-    description: "DeFi-Enabled NFT Marketplace for Cardano",
+    description: "Lovelace Cardano NFT Platform",
   },
   pathPrefix: "/lovelace",
   plugins: [
-    // {
-    //   resolve: `gatsby-plugin-compile-es6-packages`,
-    //   options: {
-    //     modules: [`xhr2-cookies`]
-    //   }
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-184384847-6",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-less`,
       options: {
--- src/components/seo.js
+++ src/components/seo.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Helmet } from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
 
-const Seo = ({ description = '', lang = 'en', meta = [], title, image }) => {
+const Seo = ({ description = 'Lovelace Cardano NFT Platform', lang = 'en', meta = [], title = 'Lovelace', image = 'https://i.ibb.co/ygTYZ96/logo-lovelace-icon-flat-color.png' }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
